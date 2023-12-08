37d007c3b50e1c5100ee67ac8b72ec8f911d2b56
Anna Sokolova <annasprite555@gmail.com>
2023-12-08 14:39:36 +0200

    before replace grid to flex in amenities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./css/style.scss";
+
+// $(document).ready(function(){
+//   $(".hamburger").click(function(){
+//     $(this).toggleClass("is-active");
+//   });
+// });
+console.log('before document load');
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  const body = document.querySelector('body');
+  const menuBtn = body.querySelector('.hamburger');
+  const menuWrapper = body.querySelector('.header__inner');
+  
+  menuBtn.addEventListener('click', function (e) {
+    menuBtn.classList.toggle('is-active');
+    menuWrapper.classList.toggle('is-active');
+    body.classList.toggle('menu-is-active');
+  })
+});
